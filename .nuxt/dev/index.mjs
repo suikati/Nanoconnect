e3d4b40f025fdfa6a0e638496a9b6bc605b8ce6e
suikati <suikatikai@gmail.com>
2025-08-23 21:47:47 +0900

    init

--- .nuxt/dev/index.mjs
+++ .nuxt/dev/index.mjs
@@ -1,42 +1,42 @@
 import process from 'node:process';globalThis._importMeta_={url:import.meta.url,env:process.env};import { tmpdir } from 'node:os';
-import { defineEventHandler, handleCacheHeaders, splitCookiesString, createEvent, fetchWithEvent, isEvent, eventHandler, setHeaders, sendRedirect, proxyRequest, getRequestHeader, setResponseHeaders, setResponseStatus, send, getRequestHeaders, setResponseHeader, appendResponseHeader, getRequestURL, getResponseHeader, removeResponseHeader, createError, getQuery as getQuery$1, readBody, createApp, createRouter as createRouter$1, toNodeListener, lazyEventHandler, getResponseStatus, getRouterParam, getResponseStatusText } from 'file:///home/projects/nuxt-starter-bvepbqhq/node_modules/.pnpm/h3@1.15.4/node_modules/h3/dist/index.mjs';
+import { defineEventHandler, handleCacheHeaders, splitCookiesString, createEvent, fetchWithEvent, isEvent, eventHandler, setHeaders, sendRedirect, proxyRequest, getRequestHeader, setResponseHeaders, setResponseStatus, send, getRequestHeaders, setResponseHeader, appendResponseHeader, getRequestURL, getResponseHeader, removeResponseHeader, createError, getQuery as getQuery$1, readBody, createApp, createRouter as createRouter$1, toNodeListener, lazyEventHandler, getResponseStatus, getRouterParam, getResponseStatusText } from 'file:///home/suikati/Nanosuke-Linka/node_modules/.pnpm/h3@1.15.4/node_modules/h3/dist/index.mjs';
 import { Server } from 'node:http';
 import { resolve, dirname, join } from 'node:path';
 import nodeCrypto from 'node:crypto';
 import { parentPort, threadId } from 'node:worker_threads';
-import { escapeHtml } from 'file:///home/projects/nuxt-starter-bvepbqhq/node_modules/.pnpm/@vue+shared@3.5.18/node_modules/@vue/shared/dist/shared.cjs.js';
-import { createRenderer, getRequestDependencies, getPreloadLinks, getPrefetchLinks } from 'file:///home/projects/nuxt-starter-bvepbqhq/node_modules/.pnpm/vue-bundle-renderer@2.1.2/node_modules/vue-bundle-renderer/dist/runtime.mjs';
-import { parseURL, withoutBase, joinURL, getQuery, withQuery, withTrailingSlash, decodePath, withLeadingSlash, withoutTrailingSlash, joinRelativeURL } from 'file:///home/projects/nuxt-starter-bvepbqhq/node_modules/.pnpm/ufo@1.6.1/node_modules/ufo/dist/index.mjs';
-import { renderToString } from 'file:///home/projects/nuxt-starter-bvepbqhq/node_modules/.pnpm/vue@3.5.18_typescript@5.9.2/node_modules/vue/server-renderer/index.mjs';
-import destr, { destr as destr$1 } from 'file:///home/projects/nuxt-starter-bvepbqhq/node_modules/.pnpm/destr@2.0.5/node_modules/destr/dist/index.mjs';
-import { createHooks } from 'file:///home/projects/nuxt-starter-bvepbqhq/node_modules/.pnpm/hookable@5.5.3/node_modules/hookable/dist/index.mjs';
-import { createFetch, Headers as Headers$1 } from 'file:///home/projects/nuxt-starter-bvepbqhq/node_modules/.pnpm/ofetch@1.4.1/node_modules/ofetch/dist/node.mjs';
-import { fetchNodeRequestHandler, callNodeRequestHandler } from 'file:///home/projects/nuxt-starter-bvepbqhq/node_modules/.pnpm/node-mock-http@1.0.2/node_modules/node-mock-http/dist/index.mjs';
-import { createStorage, prefixStorage } from 'file:///home/projects/nuxt-starter-bvepbqhq/node_modules/.pnpm/unstorage@1.16.1_db0@0.3.2_ioredis@5.7.0/node_modules/unstorage/dist/index.mjs';
-import unstorage_47drivers_47fs from 'file:///home/projects/nuxt-starter-bvepbqhq/node_modules/.pnpm/unstorage@1.16.1_db0@0.3.2_ioredis@5.7.0/node_modules/unstorage/drivers/fs.mjs';
-import { digest } from 'file:///home/projects/nuxt-starter-bvepbqhq/node_modules/.pnpm/ohash@2.0.11/node_modules/ohash/dist/index.mjs';
-import { klona } from 'file:///home/projects/nuxt-starter-bvepbqhq/node_modules/.pnpm/klona@2.0.6/node_modules/klona/dist/index.mjs';
-import defu, { defuFn } from 'file:///home/projects/nuxt-starter-bvepbqhq/node_modules/.pnpm/defu@6.1.4/node_modules/defu/dist/defu.mjs';
-import { snakeCase } from 'file:///home/projects/nuxt-starter-bvepbqhq/node_modules/.pnpm/scule@1.3.0/node_modules/scule/dist/index.mjs';
-import { getContext } from 'file:///home/projects/nuxt-starter-bvepbqhq/node_modules/.pnpm/unctx@2.4.1/node_modules/unctx/dist/index.mjs';
-import { toRouteMatcher, createRouter } from 'file:///home/projects/nuxt-starter-bvepbqhq/node_modules/.pnpm/radix3@1.1.2/node_modules/radix3/dist/index.mjs';
+import { escapeHtml } from 'file:///home/suikati/Nanosuke-Linka/node_modules/.pnpm/@vue+shared@3.5.18/node_modules/@vue/shared/dist/shared.cjs.js';
+import { createRenderer, getRequestDependencies, getPreloadLinks, getPrefetchLinks } from 'file:///home/suikati/Nanosuke-Linka/node_modules/.pnpm/vue-bundle-renderer@2.1.2/node_modules/vue-bundle-renderer/dist/runtime.mjs';
+import { parseURL, withoutBase, joinURL, getQuery, withQuery, withTrailingSlash, decodePath, withLeadingSlash, withoutTrailingSlash, joinRelativeURL } from 'file:///home/suikati/Nanosuke-Linka/node_modules/.pnpm/ufo@1.6.1/node_modules/ufo/dist/index.mjs';
+import { renderToString } from 'file:///home/suikati/Nanosuke-Linka/node_modules/.pnpm/vue@3.5.18_typescript@5.9.2/node_modules/vue/server-renderer/index.mjs';
+import destr, { destr as destr$1 } from 'file:///home/suikati/Nanosuke-Linka/node_modules/.pnpm/destr@2.0.5/node_modules/destr/dist/index.mjs';
+import { createHooks } from 'file:///home/suikati/Nanosuke-Linka/node_modules/.pnpm/hookable@5.5.3/node_modules/hookable/dist/index.mjs';
+import { createFetch, Headers as Headers$1 } from 'file:///home/suikati/Nanosuke-Linka/node_modules/.pnpm/ofetch@1.4.1/node_modules/ofetch/dist/node.mjs';
+import { fetchNodeRequestHandler, callNodeRequestHandler } from 'file:///home/suikati/Nanosuke-Linka/node_modules/.pnpm/node-mock-http@1.0.2/node_modules/node-mock-http/dist/index.mjs';
+import { createStorage, prefixStorage } from 'file:///home/suikati/Nanosuke-Linka/node_modules/.pnpm/unstorage@1.16.1_db0@0.3.2_ioredis@5.7.0/node_modules/unstorage/dist/index.mjs';
+import unstorage_47drivers_47fs from 'file:///home/suikati/Nanosuke-Linka/node_modules/.pnpm/unstorage@1.16.1_db0@0.3.2_ioredis@5.7.0/node_modules/unstorage/drivers/fs.mjs';
+import { digest } from 'file:///home/suikati/Nanosuke-Linka/node_modules/.pnpm/ohash@2.0.11/node_modules/ohash/dist/index.mjs';
+import { klona } from 'file:///home/suikati/Nanosuke-Linka/node_modules/.pnpm/klona@2.0.6/node_modules/klona/dist/index.mjs';
+import defu, { defuFn } from 'file:///home/suikati/Nanosuke-Linka/node_modules/.pnpm/defu@6.1.4/node_modules/defu/dist/defu.mjs';
+import { snakeCase } from 'file:///home/suikati/Nanosuke-Linka/node_modules/.pnpm/scule@1.3.0/node_modules/scule/dist/index.mjs';
+import { getContext } from 'file:///home/suikati/Nanosuke-Linka/node_modules/.pnpm/unctx@2.4.1/node_modules/unctx/dist/index.mjs';
+import { toRouteMatcher, createRouter } from 'file:///home/suikati/Nanosuke-Linka/node_modules/.pnpm/radix3@1.1.2/node_modules/radix3/dist/index.mjs';
 import { readFile } from 'node:fs/promises';
-import consola, { consola as consola$1 } from 'file:///home/projects/nuxt-starter-bvepbqhq/node_modules/.pnpm/consola@3.4.2/node_modules/consola/dist/index.mjs';
-import { ErrorParser } from 'file:///home/projects/nuxt-starter-bvepbqhq/node_modules/.pnpm/youch-core@0.3.3/node_modules/youch-core/build/index.js';
-import { Youch } from 'file:///home/projects/nuxt-starter-bvepbqhq/node_modules/.pnpm/youch@4.1.0-beta.8/node_modules/youch/build/index.js';
-import { SourceMapConsumer } from 'file:///home/projects/nuxt-starter-bvepbqhq/node_modules/.pnpm/source-map@0.7.6/node_modules/source-map/source-map.js';
+import consola, { consola as consola$1 } from 'file:///home/suikati/Nanosuke-Linka/node_modules/.pnpm/consola@3.4.2/node_modules/consola/dist/index.mjs';
+import { ErrorParser } from 'file:///home/suikati/Nanosuke-Linka/node_modules/.pnpm/youch-core@0.3.3/node_modules/youch-core/build/index.js';
+import { Youch } from 'file:///home/suikati/Nanosuke-Linka/node_modules/.pnpm/youch@4.1.0-beta.8/node_modules/youch/build/index.js';
+import { SourceMapConsumer } from 'file:///home/suikati/Nanosuke-Linka/node_modules/.pnpm/source-map@0.7.6/node_modules/source-map/source-map.js';
 import { AsyncLocalStorage } from 'node:async_hooks';
-import { stringify, uneval } from 'file:///home/projects/nuxt-starter-bvepbqhq/node_modules/.pnpm/devalue@5.1.1/node_modules/devalue/index.js';
-import { captureRawStackTrace, parseRawStackTrace } from 'file:///home/projects/nuxt-starter-bvepbqhq/node_modules/.pnpm/errx@0.1.0/node_modules/errx/dist/index.js';
-import { isVNode, toValue, isRef } from 'file:///home/projects/nuxt-starter-bvepbqhq/node_modules/.pnpm/vue@3.5.18_typescript@5.9.2/node_modules/vue/index.mjs';
+import { stringify, uneval } from 'file:///home/suikati/Nanosuke-Linka/node_modules/.pnpm/devalue@5.1.1/node_modules/devalue/index.js';
+import { captureRawStackTrace, parseRawStackTrace } from 'file:///home/suikati/Nanosuke-Linka/node_modules/.pnpm/errx@0.1.0/node_modules/errx/dist/index.js';
+import { isVNode, toValue, isRef } from 'file:///home/suikati/Nanosuke-Linka/node_modules/.pnpm/vue@3.5.18_typescript@5.9.2/node_modules/vue/index.mjs';
 import { promises } from 'node:fs';
 import { fileURLToPath } from 'node:url';
-import { dirname as dirname$1, resolve as resolve$1 } from 'file:///home/projects/nuxt-starter-bvepbqhq/node_modules/.pnpm/pathe@2.0.3/node_modules/pathe/dist/index.mjs';
-import { createHead as createHead$1, propsToString, renderSSRHead } from 'file:///home/projects/nuxt-starter-bvepbqhq/node_modules/.pnpm/unhead@2.0.14/node_modules/unhead/dist/server.mjs';
-import { DeprecationsPlugin, PromisesPlugin, TemplateParamsPlugin, AliasSortingPlugin } from 'file:///home/projects/nuxt-starter-bvepbqhq/node_modules/.pnpm/unhead@2.0.14/node_modules/unhead/dist/plugins.mjs';
-import { walkResolver } from 'file:///home/projects/nuxt-starter-bvepbqhq/node_modules/.pnpm/unhead@2.0.14/node_modules/unhead/dist/utils.mjs';
+import { dirname as dirname$1, resolve as resolve$1 } from 'file:///home/suikati/Nanosuke-Linka/node_modules/.pnpm/pathe@2.0.3/node_modules/pathe/dist/index.mjs';
+import { createHead as createHead$1, propsToString, renderSSRHead } from 'file:///home/suikati/Nanosuke-Linka/node_modules/.pnpm/unhead@2.0.14/node_modules/unhead/dist/server.mjs';
+import { DeprecationsPlugin, PromisesPlugin, TemplateParamsPlugin, AliasSortingPlugin } from 'file:///home/suikati/Nanosuke-Linka/node_modules/.pnpm/unhead@2.0.14/node_modules/unhead/dist/plugins.mjs';
+import { walkResolver } from 'file:///home/suikati/Nanosuke-Linka/node_modules/.pnpm/unhead@2.0.14/node_modules/unhead/dist/utils.mjs';
 
-const serverAssets = [{"baseName":"server","dir":"/home/projects/nuxt-starter-bvepbqhq/server/assets"}];
+const serverAssets = [{"baseName":"server","dir":"/home/suikati/Nanosuke-Linka/server/assets"}];
 
 const assets$1 = createStorage();
 
@@ -48,11 +48,11 @@ const storage = createStorage({});
 
 storage.mount('/assets', assets$1);
 
-storage.mount('root', unstorage_47drivers_47fs({"driver":"fs","readOnly":true,"base":"/home/projects/nuxt-starter-bvepbqhq","watchOptions":{"ignored":[null]}}));
-storage.mount('src', unstorage_47drivers_47fs({"driver":"fs","readOnly":true,"base":"/home/projects/nuxt-starter-bvepbqhq/server","watchOptions":{"ignored":[null]}}));
-storage.mount('build', unstorage_47drivers_47fs({"driver":"fs","readOnly":false,"base":"/home/projects/nuxt-starter-bvepbqhq/.nuxt"}));
-storage.mount('cache', unstorage_47drivers_47fs({"driver":"fs","readOnly":false,"base":"/home/projects/nuxt-starter-bvepbqhq/.nuxt/cache"}));
-storage.mount('data', unstorage_47drivers_47fs({"driver":"fs","base":"/home/projects/nuxt-starter-bvepbqhq/.data/kv"}));
+storage.mount('root', unstorage_47drivers_47fs({"driver":"fs","readOnly":true,"base":"/home/suikati/Nanosuke-Linka","watchOptions":{"ignored":[null]}}));
+storage.mount('src', unstorage_47drivers_47fs({"driver":"fs","readOnly":true,"base":"/home/suikati/Nanosuke-Linka/server","watchOptions":{"ignored":[null]}}));
+storage.mount('build', unstorage_47drivers_47fs({"driver":"fs","readOnly":false,"base":"/home/suikati/Nanosuke-Linka/.nuxt"}));
+storage.mount('cache', unstorage_47drivers_47fs({"driver":"fs","readOnly":false,"base":"/home/suikati/Nanosuke-Linka/.nuxt/cache"}));
+storage.mount('data', unstorage_47drivers_47fs({"driver":"fs","base":"/home/suikati/Nanosuke-Linka/.data/kv"}));
 
 function useStorage(base = "") {
   return base ? prefixStorage(storage, base) : storage;
@@ -1006,7 +1006,7 @@ async function errorHandler(error, event) {
   // H3 will handle fallback
 }
 
-const rootDir = "/home/projects/nuxt-starter-bvepbqhq";
+const rootDir = "/home/suikati/Nanosuke-Linka";
 
 const appHead = {"meta":[{"name":"viewport","content":"width=device-width, initial-scale=1"},{"charset":"utf-8"}],"link":[],"style":[],"script":[],"noscript":[]};
 
@@ -1025,7 +1025,7 @@ const devReducers = {
   URL: (data) => data instanceof URL ? data.toString() : void 0
 };
 const asyncContext = getContext("nuxt-dev", { asyncContext: true, AsyncLocalStorage });
-const _75wM6oI9Oz8lCxsurcvPXnF1zAZYoYvXheOhI4E4YPA = (nitroApp) => {
+const _9SfaRpnkcf6SZ_wHaOGydnAOFxF4VGlG7LYNAJI4kwM = (nitroApp) => {
   const handler = nitroApp.h3App.handler;
   nitroApp.h3App.handler = (event) => {
     return asyncContext.callAsync({ logs: [], event }, () => handler(event));
@@ -1095,7 +1095,7 @@ function onConsoleLog(callback) {
 }
 
 const plugins = [
-  _75wM6oI9Oz8lCxsurcvPXnF1zAZYoYvXheOhI4E4YPA
+  _9SfaRpnkcf6SZ_wHaOGydnAOFxF4VGlG7LYNAJI4kwM
 ];
 
 const assets = {};
@@ -1123,7 +1123,7 @@ function getAsset (id) {
 
 const METHODS = /* @__PURE__ */ new Set(["HEAD", "GET"]);
 const EncodingMap = { gzip: ".gz", br: ".br" };
-const _1YPEd0 = eventHandler((event) => {
+const _vPdKoP = eventHandler((event) => {
   if (event.method && !METHODS.has(event.method)) {
     return;
   }
@@ -1258,8 +1258,8 @@ function publicAssetsURL(...path) {
 
 const APP_ROOT_OPEN_TAG = `<${appRootTag}${propsToString(appRootAttrs)}>`;
 const APP_ROOT_CLOSE_TAG = `</${appRootTag}>`;
-const getServerEntry = () => import('file:///home/projects/nuxt-starter-bvepbqhq/.nuxt//dist/server/server.mjs').then((r) => r.default || r);
-const getClientManifest = () => import('file:///home/projects/nuxt-starter-bvepbqhq/.nuxt//dist/server/client.manifest.mjs').then((r) => r.default || r).then((r) => typeof r === "function" ? r() : r);
+const getServerEntry = () => import('file:///home/suikati/Nanosuke-Linka/.nuxt//dist/server/server.mjs').then((r) => r.default || r);
+const getClientManifest = () => import('file:///home/suikati/Nanosuke-Linka/.nuxt//dist/server/client.manifest.mjs').then((r) => r.default || r).then((r) => typeof r === "function" ? r() : r);
 const getSSRRenderer = lazyCachedFunction(async () => {
   const manifest = await getClientManifest();
   if (!manifest) {
@@ -1505,13 +1505,13 @@ async function getIslandContext(event) {
   return ctx;
 }
 
-const _lazy_5pxp8N = () => Promise.resolve().then(function () { return renderer$1; });
+const _lazy_nM41w4 = () => Promise.resolve().then(function () { return renderer$1; });
 
 const handlers = [
-  { route: '', handler: _1YPEd0, lazy: false, middleware: true, method: undefined },
-  { route: '/__nuxt_error', handler: _lazy_5pxp8N, lazy: true, middleware: false, method: undefined },
+  { route: '', handler: _vPdKoP, lazy: false, middleware: true, method: undefined },
+  { route: '/__nuxt_error', handler: _lazy_nM41w4, lazy: true, middleware: false, method: undefined },
   { route: '/__nuxt_island/**', handler: _SxA8c9, lazy: false, middleware: false, method: undefined },
-  { route: '/**', handler: _lazy_5pxp8N, lazy: true, middleware: false, method: undefined }
+  { route: '/**', handler: _lazy_nM41w4, lazy: true, middleware: false, method: undefined }
 ];
 
 function createNitroApp() {
