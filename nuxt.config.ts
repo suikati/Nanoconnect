610845286854269f620b39c36a3d53bb391ac979
suikati <suikatikai@gmail.com>
2025-08-23 22:06:11 +0900

    dependenciesをインストール

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: false }
+  devtools: { enabled: false },
+  runtimeConfig: {
+    public: {
+      supabaseUrl: process.env.SUPABASE_URL,
+      supabaseKey: process.env.SUPABASE_KEY,
+    },
+  },
 })
