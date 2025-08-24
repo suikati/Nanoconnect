1985964ecfe05272ae0db2b3dde33b958833e0ae
Suika <suikatikai@gmail.com>
2025-08-24 13:12:00 +0900

    全てのプレゼンターおよびオーディエンス関連のコンポーネントを削除

--- pages/presenter/[roomCode]/control.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <h1>プレゼンターコントロール</h1>
-    <p>現在のスライド番号: {{ currentSlideNumber }}</p>
-    <button @click="previousSlide" :disabled="currentSlideIndex === 0">前のスライド</button>
-    <button @click="nextSlide" :disabled="currentSlideIndex === totalSlides - 1">次のスライド</button>
-  </div>
-</template>
