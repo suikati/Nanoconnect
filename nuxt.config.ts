bad1630a11ce1cdf0e34187576471e4e75125527
suikati <suikatikai@gmail.com>
2025-08-24 15:21:18 +0900

    tailwindcssのインストール

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
+  modules: ['@nuxtjs/tailwindcss'],
   runtimeConfig: {
     public: {
       // Firebase public config (safe to be public: API key is not a secret)
